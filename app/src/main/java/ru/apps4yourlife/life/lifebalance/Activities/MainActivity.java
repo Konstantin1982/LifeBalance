4567b7f23fd3bd66a6c931c2b93da08fa3d22e5b
Konstantin Sharafutdinov <konstantin.sharafutdinov@icmcapital.co.uk>
2018-11-08 01:20:16 +0300

    initial commit

---
+++ app/src/main/java/ru/apps4yourlife/life/lifebalance/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.apps4yourlife.life.lifebalance.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.apps4yourlife.life.lifebalance.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
