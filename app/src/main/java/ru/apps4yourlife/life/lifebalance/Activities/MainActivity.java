2d38fe470150cd8c98e6c184f5917eabdb5e082d
Konstantin Sharafutdinov <konstantin.sharafutdinov@icmcapital.co.uk>
2018-11-09 13:15:26 +0300

    button design

--- app/src/main/java/ru/apps4yourlife/life/lifebalance/Activities/MainActivity.java
+++ app/src/main/java/ru/apps4yourlife/life/lifebalance/Activities/MainActivity.java
@@ -2,6 +2,8 @@ package ru.apps4yourlife.life.lifebalance.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import ru.apps4yourlife.life.lifebalance.R;
 
@@ -11,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void wishList_click(View view) {
+        Toast.makeText(this, "Wish List clicked", Toast.LENGTH_SHORT).show();
     }
 }
